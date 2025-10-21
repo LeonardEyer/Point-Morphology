ea4c663383f11af18ffde1ca040dd46cd68c22b6
Leonard Eyer <leonardeyer@fastmail.de>
2025-10-21 10:20:03 +0200

    Draw some pointcloud

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include <polyscope/types.h>
+#include <polyscope/utilities.h>
+#include <polyscope/point_cloud.h>
 #include <polyscope/polyscope.h>
 
-
 int main() {
+  
   // Options
   polyscope::options::autocenterStructures = true;
   polyscope::view::windowWidth = 1024;
@@ -9,6 +12,18 @@ int main() {
 
   // Initialize polyscope
   polyscope::init();
+  
+  std::vector<glm::vec3> points;
+
+  for (auto i = 0; i < 3000; i++) {
+    points.push_back(glm::vec3{polyscope::randomUnit() - .5,
+                               polyscope::randomUnit() - .5,
+                               polyscope::randomUnit() - .5});
+  }
+
+  auto *psCloud = polyscope::registerPointCloud("some points", points);
+  psCloud->setPointRadius(0.004);
+  psCloud->setPointRenderMode(polyscope::PointRenderMode::Sphere);
 
   polyscope::show();
 }
