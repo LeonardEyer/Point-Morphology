3e62400cfee2b94ac1e3e5f319884f10329066bf
Leonard Eyer <leonardeyer@fastmail.de>
2025-12-05 10:00:21 +0100

    Add conversion method to eigen

--- inc/Utils.hpp
+++ inc/Utils.hpp
@@ -9,7 +9,10 @@ inline auto concat(const glm::vec3 &a, const glm::vec3 &b) {
   return Eigen::Vector<float, 6>{a.x, a.y, a.z, b.x, b.y, b.z};
 }
 
-inline glm::vec3 to_glm(Eigen::Vector3f x) { return {x.x(), x.y(), x.z()}; }
+inline glm::vec3 to_glm(const Eigen::Vector3f &x) {
+  return {x.x(), x.y(), x.z()};
+}
+inline Eigen::Vector3f to_eigen(const glm::vec3 &x) { return {x.x, x.y, x.z}; }
 
 using V6f = Eigen::Vector<float, 6>;
 using PointNormal = V6f;
